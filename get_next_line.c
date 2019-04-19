0c436ba5da20e877d15f97f8ebb5d3ecf99b086d
Fen <fepinson@student.42.us.org>
2019-04-19 14:03:57 +0200

    clean

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fepinson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/08 23:26:18 by fepinson          #+#    #+#             */
-/*   Updated: 2019/04/19 11:25:23 by fepinson         ###   ########.fr       */
+/*   Updated: 2019/04/19 14:01:36 by fepinson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,28 @@
 
 int		ft_split_line(char **line, char *s, char **rest)
 {
-	size_t	i;
-	size_t	j;
+	size_t	i[2];
 
-	j = ft_strlen(s);
-	if ((i = (size_t)(ft_strchr(s, '\n') - s)))
+	i[1] = ft_strlen(s);
+	if ((i[0] = (size_t)(ft_strchr(s, '\n') - s)))
 	{
-		j -= i;
-		if (!(*line = ft_memalloc(i + 1)) 
-				|| !(*rest = ft_memalloc(j + 1)))
+		i[1] -= i[0];
+		if (!(*line = ft_memalloc(i[0] + 1))
+			|| !(*rest = ft_memalloc(i[1] + 1)))
 			return (-1);
-		j = i;
-		while (--i)
-			(*line)[i] = s[i];
-		*line[i] = s[i];
-		while (s[++j])
-			(*rest)[i++] = s[j];
+		i[1] = i[0];
+		while (--i[0])
+			(*line)[i[0]] = s[i[0]];
+		*line[i[0]] = s[i[0]];
+		while (s[++i[1]])
+			(*rest)[i[0]++] = s[i[1]];
 	}
 	else
 	{
 		if (!(*line = ft_memalloc(1)))
 			return (-1);
-		if (j)
-			if (!(*rest = ft_strdup(s + 1)))
-				return (-1);
+		if (i[1] && !(*rest = ft_strdup(s + 1)))
+			return (-1);
 	}
 	free(s);
 	return (1);
@@ -50,15 +48,14 @@ int		get_next_line(const int fd, char **line)
 	char			*s;
 	int				rt;
 
-	if (fd < 0 || fd > OPEN_MAX || !line )
+	if (fd < 0 || fd > OPEN_MAX || !line)
 		return (-1);
 	if (rest[fd] && ft_strchr(rest[fd], '\n'))
 		return (ft_split_line(line, rest[fd], &rest[fd]));
-	else if (rest[fd])
-		*line = rest[fd];
-	else
-		if (!(*line = ft_memalloc(1)))
-			return (-1);
+	else if ((*line = rest[fd]))
+		rest[fd] = NULL;
+	else if (!(*line = ft_memalloc(1)))
+		return (-1);
 	if (!(buf = ft_memalloc(BUFF_SIZE + 1)))
 		return (-1);
 	while (!ft_strchr(*line, '\n') && (rt = read(fd, buf, BUFF_SIZE)) > 0)
@@ -69,7 +66,6 @@ int		get_next_line(const int fd, char **line)
 		free(s);
 	}
 	free(buf);
-	if (rt)
-		rt = ft_split_line(line, *line, &rest[fd]);
-	return (rt);
+	rt = rt > 0 ? ft_split_line(line, *line, &rest[fd]) : rt;
+	return (!rt && **line ? 1 : rt);
 }
