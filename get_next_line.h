ed632075068e96603bf3a98bf69fa06d6c9fc74a
Fen <fepinson@student.42.us.org>
2019-04-23 12:50:33 +0200

    cleaned

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fepinson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/08 23:27:38 by fepinson          #+#    #+#             */
-/*   Updated: 2019/04/19 14:03:30 by fepinson         ###   ########.fr       */
+/*   Updated: 2019/04/19 14:08:37 by fepinson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <fcntl.h>
 # include <limits.h>
 
-# define BUFF_SIZE 1
+# define BUFF_SIZE 32
 
 typedef	struct	s_gnl
 {
